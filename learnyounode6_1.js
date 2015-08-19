f9bddef80eef7ca3a6c14ea899bd79751f24a8ce
bbouley <b.bouley@hotmail.co.uk>
2015-08-19 15:31:31 -0600

    added exercise 6

---
+++ learnyounode6_1.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var module = require('./learnyounode6_2.js');
+
+
+module(process.argv[2], process.argv[3], function(err, data){
+  if(err){
+    console.log(err);
+  } else {
+    for (var i = 0; i < data.length; i++) {
+      console.log(data[i]);
+    }
+  }
+});
---
+++ learnyounode6_2.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+
+module.exports = function(dirName, extension, callback){
+  var dataArray =[];
+  fs.readdir(dirName, function(err, data){
+    if(err){
+      return callback(err);
+    } else {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].indexOf('.'+extension) >= 0){
+          dataArray.push(data[i]);
+        }
+      }
+    }
+    callback(null, dataArray);
+  });
+};
+
+
